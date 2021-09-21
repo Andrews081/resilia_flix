a2da82dbedd3d8dc86f39b2bd15746cc2c8f0342
Elder Schmoeller <elderschmoellerls@gmail.com>
2021-09-21 14:02:54 -0300

    FEAT: Adicionando página de Cadastro.

---
+++ js/cadastro2.js
@@ -0,0 +1,5 @@
+// retornar para a tela de Login
+let btnBacklogin = document.getElementById('loginBack');
+btnEnviar.addEventListener('click', function() {
+    return location = ('./html/login.html');
+})
