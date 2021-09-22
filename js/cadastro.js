4d4b8a4a2c131bc1f825e5c5549f26b2d61b422d
Elder Schmoeller <elderschmoellerls@gmail.com>
2021-09-21 21:36:59 -0300

    FEAT: Alteração comments

--- js/cadastro.js
+++ js/cadastro.js
@@ -4,11 +4,10 @@ $('btn').click(function (evento) {
 })
 
 let btnEnviar = document.getElementById('enviar');
-btnEnviar.addEventListener('click', function() {
+btnEnviar.addEventListener('click', function () {
     console.log('caque coisa')
     return location = ('../html/cadastro2.html');
 })
-//Seperação números CEP
 const inputValue = document.querySelector("#cep");
 let zipCode = "";
 
@@ -22,69 +21,53 @@ inputValue.addEventListener("keyup", () => {
 
 $(document).ready(function () {
 
-
     function limpa_formulário_cep() {
-        // Limpa valores do formulário de cep.
         $("#rua").val("");
         $("#bairro").val("");
         $("#cidade").val("");
-        // $('#numero').val("");
         $("#uf").val("");
     }
 
-    //Quando o campo cep perde o foco.
     $("#cep").blur(function () {
 
-        //Nova variável "cep" somente com dígitos.
         var cep = $(this).val().replace(/\D/g, '');
 
-        //Verifica se campo cep possui valor informado.
         if (cep != "") {
 
-            //Expressão regular para validar o CEP.
             var validacep = /^[0-9]{8}$/;
 
-            //Valida o formato do CEP.
             if (validacep.test(cep)) {
 
-                //Preenche os campos com "..." enquanto consulta webservice.
                 $("#rua").val("...");
                 $("#bairro").val("...");
                 $("#cidade").val("...");
-                // $('#numero').val("...");
                 $("#uf").val("...");
 
-                //Consulta o webservice viacep.com.br/
                 $.getJSON("https://viacep.com.br/ws/" + cep + "/json/?callback=?", function (dados) {
 
                     if (!("erro" in dados)) {
-                        //Atualiza os campos com os valores da consulta.
                         $("#rua").val(dados.logradouro);
                         $("#bairro").val(dados.bairro);
                         $("#cidade").val(dados.localidade);
-                        $('#numero').val(dados.numero)
+                        $('#numero').val(dados.numero);
                         $("#uf").val(dados.uf);
                     }
                     else {
-                        //CEP pesquisado não foi encontrado.
                         limpa_formulário_cep();
                         alert("CEP não encontrado.");
                     }
                 });
             }
             else {
-                //cep é inválido.
                 limpa_formulário_cep();
                 alert("Formato de CEP inválido.");
             }
         }
         else {
-            //cep sem valor, limpa formulário.
             limpa_formulário_cep();
         }
     });
 });
-
 
 function verificarNomes() {
 
@@ -117,14 +100,6 @@ function verificarNomes() {
         }
     }
 }
-
-
-// Enviar para página de cadastro realizado
-// let btnEnviar = document.getElementById('enviar');
-// btnEnviar.addEventListener('click', function() {
-//     console.log('caque coisa')
-//     return location = ('../html/cadastro2.html');
-// })
 
 
 
--- js/cadastro2.js
+++ js/cadastro2.js
@@ -1,4 +1,3 @@
-// retornar para a tela de Login
 let btnBacklogin = document.getElementById('loginBack');
 btnEnviar.addEventListener('click', function() {
     return location = ('./html/login.html');
